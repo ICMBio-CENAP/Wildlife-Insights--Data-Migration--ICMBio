385b5f76145002494074643f4df492abe2e62939
unknown <elildojr@gmail.com>
2020-07-13 17:25:29 -0300

    some changes for Maraca

--- Transformation_Code/Wild.ID/Part1-Prep-data-maraca.R
+++ Transformation_Code/Wild.ID/Part1-Prep-data-maraca.R
@@ -18,43 +18,47 @@ check.coord(maraca)
 # attach for next steps
 attach(maraca)
 
+cts.id <- as.character(cts.id)
+image.id <- as.character(image.id)
+n_last <- 12 # Specify number of characters to extract (for image ID)
+
 # create "Image" file from csv
-Image <- tibble("Project ID" = "EEM", "Deployment ID" = Camera.Trap.Name, 
-                "Image ID" = paste(Camera.Trap.Name, Raw.Name, sep="_"),
-                "Location" =  paste(Camera.Trap.Name, Raw.Name, sep="/"), 
-                "Photo Type" =  Photo.Type, "Photo Type Identified by" =  Person.Identifying.the.Photo,
-                "Genus Species" = paste(Genus, Species, sep=" "), "Uncertainty" = NA,
-                "IUCN Identification Number" = NA, "Date_Time Captured" = paste(Photo.Date, Photo.time, sep=" "),
-                "Age" = NA,  "Sex" = NA, "Individual ID" = NA,  "Count" =  Number.of.Animals,
+Image <- tibble("Project ID" = "EEM", "Deployment ID" = substr(cts.id, 9, nchar(cts.id)), 
+                "Image ID" = substr(image.id, nchar(image.id) - n_last + 1, nchar(image.id)),
+                "Location" =  paste(substr(cts.id, 9, nchar(cts.id)), substr(image.id, nchar(image.id) - n_last + 1, nchar(image.id)), sep="/"), 
+                "Photo Type" =  photo.type, "Photo Type Identified by" =  identification,
+                "Genus Species" = species, "Uncertainty" = uncertainty,
+                "IUCN Identification Number" = NA, "Date_Time Captured" = date.time,
+                "Age" = NA,  "Sex" = NA, "Individual ID" = NA,  "Count" =  count,
                 "Animal recognizable (Y/N)" = NA, "Individual Animal Notes" = NA) 
 write.csv(Image, here("Datasets", "maraca", "Image.csv"), row.names = FALSE)
 
 
 # create "Deployment" file from csv
-Deployment <- tibble("Deployment ID" = paste("maraca", Camera.Trap.Name, sep="_"), "Event Name" = 2017,
-                    "Array Name (Optional)" = substr(Camera.Trap.Name, 1, stop = 8),"Deployment Location ID" = Camera.Trap.Name,
-                    "Longitude Resolution" = Longitude, "Latitude Resolution" = Latitude, 
-                    "Camera Deployment Begin Date" = Camera.Start.Date, "Camera Deployment End Date" = Camera.End.Date,
+Deployment <- tibble("Deployment ID" = paste("Maraca", substr(cts.id, 9, nchar(cts.id)), sep="_"), "Event Name" = 2018,
+                    "Array Name (Optional)" = substr(cts.id, 9, 16),"Deployment Location ID" = substr(cts.id, 9, nchar(cts.id)),
+                    "Longitude Resolution" = lon, "Latitude Resolution" = lat, 
+                    "Camera Deployment Begin Date" = as.Date(start.date, format = "%d/%m/%Y"), "Camera Deployment End Date" = as.Date(end.date, format="%d/%m/%Y"),
                     "Bait Type" = "No Bait", "Bait Description" = NA, "Feature Type" = NA,
-                    "Feature Type Methodology" = NA, "Camera ID" =  Camera.Serial.Number,"Quiet Period Setting" = NA,
+                    "Feature Type Methodology" = NA, "Camera ID" =  camtrap.serial, "Quiet Period Setting" = NA,
                     "Restriction on Access" = NA, "Camera Failure Details" = NA,  "Camera Hardware Failure" = NA)
 write.csv(Deployment, here("Datasets", "maraca", "Deployment.csv"), row.names = FALSE)
 
 
 # create "Cameras" file from csv
-Cameras <- tibble("Project ID" = Project.Name, "Camera ID" = Camera.Serial.Number, 
-                  "Make" = Camera.Manufacturer, "Model" = Camera.Model, 
-                  "Serial Number" = Camera.Serial.Number, "Year Purchased" = 2016)
+Cameras <- tibble("Project ID" = "Maraca", "Camera ID" = camtrap.serial, 
+                  "Make" = "Bushnell", "Model" = "Trophy Cam HD", 
+                  "Serial Number" = camtrap.serial, "Year Purchased" = 2017)
 
 Cameras <- Cameras %>% distinct(`Camera ID`, .keep_all = TRUE)
 write.csv(Cameras, here("Datasets", "maraca", "Cameras.csv"), row.names = FALSE)
 
 
 # Create "Project" file from csv 
-Project <- tibble("Project ID" = "maraca", "Publish Date" = "2020-06-07", "Project Name" = Project.Name,
-                  "Project Objectives" = "Wildlife survey", "Project Owner (Organization or Individual)" = "ICMBio/CENAP",
-                  "Project Owner Email (if applicable)" = "elildojr@gmail.com",  "Principal Investigator" = "Elildo Carvalho Jr",
-                   "Principal Investigator Email" = "elildojr@gmail.com", "Project Contact" = "Elildo Carvalho Jr",  "Project Contact Email" = "elildojr@gmail.com",
+Project <- tibble("Project ID" = "EEM", "Publish Date" = "2020-06-07", "Project Name" = "Maraca",
+                  "Project Objectives" = "Long-term wildlife monitoring", "Project Owner (Organization or Individual)" = "ICMBio/CENAP",
+                  "Project Owner Email (if applicable)" = "elildojr@gmail.com",  "Principal Investigator" = "Whaldener Endo",
+                   "Principal Investigator Email" = "neotropical@gmail.com", "Project Contact" = "Elildo Carvalho Jr",  "Project Contact Email" = "elildojr@gmail.com",
                   "Country Code" = "BRA",  "Project Data Use and Constraints" = NA)
 write.csv(Project, here("Datasets", "maraca", "Project.csv"), row.names = FALSE)
 
